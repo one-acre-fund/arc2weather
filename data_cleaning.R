cf897812282f25d8a0cfec64e6e5c6c2c14306d0
Matthew Lowes <matt.lowes@oneacrefund.org>
2018-12-05 10:52:03 -0500

    pulled in and combined location boundaries, ready to subset data

--- data_cleaning.R
+++ data_cleaning.R
@@ -78,3 +78,81 @@ subsetYearMonth <- function(rList, year, month){
 }
 
 test <- subsetYearMonth(arcWeatherRaw, 1983, 1)
+
+
+# import the Kenya shapefile, focus on western Kenya and then subset for certain years
+gpsDir <- normalizePath(file.path("..", "..", "soil_grids_data"))
+
+siteGpsFiles <- list.files(paste(gpsDir, "2019", sep = "/"), pattern = ".xls")
+
+siteGpsFiles <- siteGpsFiles[!siteGpsFiles %in% c("District Offices.xlsx", "Regional Office.xlsx", "Warehouses.xlsx")]
+siteGpsFiles <- paste(gpsDir, "2019", siteGpsFiles, sep = "/")
+
+readGpsFiles <- lapply(siteGpsFiles, function(x){
+  read_xlsx(x)
+})
+
+combineGpsFiles <- as.data.frame(do.call(rbind, readGpsFiles))
+
+# investigate the projection of these data points
+siteSp <- SpatialPointsDataFrame(coords = combineGpsFiles[,c("Longitude", "Latitude")], data = combineGpsFiles, proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")) # use this to get the right map for N and P
+
+if(!file.exists(paste0(dataDir, "/GADM_2.8_KEN_adm2.rds"))){
+  keBoundaries <- getData("GADM", country='KE', level=2, path = dataDir) 
+} else {
+  keBoundaries <- readRDS(paste0(dataDir, "/GADM_2.8_KEN_adm2.rds"))
+}
+
+# I additionally want to subset this down to just western Kenya because it's too computationally intentsive to run these calculations for other areas. I'm going to subset the shape file to just western Kenya and the plot it to confirm I've done it correctly.
+# original crs for site points in Kenya = +proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0
+# original crs for ke boundaries = +proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0
+# original crs for weather data = +proj=longlat +a=6371000 +b=6371000 +no_defs
+
+# I suggest we use the weather data crs to avoid having lots of intensive reprojection
+
+siteReproject <- spTransform(siteSp, CRS("+proj=longlat +a=6371000 +b=6371000 +no_defs"))
+boundaryReproject <- spTransform(keBoundaries, CRS("+proj=longlat +a=6371000 +b=6371000 +no_defs"))
+
+weatherMap <- intersect(boundaryReproject, siteReproject)
+
+### KENYA CROP AND MASK BEFORE AGGREGATING DATA ### 
+
+# i'm still not really sure what the most expendient way is to get the data we need. 
+# I think the best sequence will be extracting the points and then subsetting down by years or months as need be depending on the use case and aggregation
+# 
+
+# This means I need a full list of gps across all countries in the right CRS. I
+# then can subset down to just the countries I need to make the data smaller.
+# This is hopefully small enough that I can then extract the points I need from
+# a complete brick and then perform the right aggregations by season, year, and month.
+
+### FIRST DOWNLOAD THE COUNTRIES OF INTEREST AND COMBINE INTO ONE SP FILE?
+# use ISO codes
+oafCountries <- c("KEN", "RWA", "UGA", "BDI", "TZA", "ZMB", "MWI") 
+
+# this imports the shape files I need.
+countrySpFiles <- lapply(oafCountries, function(x){
+  getData("GADM", country = x, level = 2, path = dataDir)
+})
+
+gadmFiles <- paste(dataDir, list.files(dataDir, pattern = "GADM"), sep = "/")
+
+# check crs to make sure the files play nicely together
+crsCheck <- lapply(gadmFiles, function(x){
+  tmp = readRDS(x)
+  coordsystem = crs(tmp)@projargs
+  return(coordsystem)
+})
+
+# then check that the results are the same
+length(unique(crsCheck))==1
+
+# combine into one sp file for subsetting the weather data
+oafArea <- do.call(bind, lapply(gadmFiles, function(x){readRDS(x)}))
+
+# this is what I now what to use to subset the large and unwieldy weather data, by decade or year if need be. use velox to speed this up!
+oafAreaReproject <- spTransform(oafArea, CRS("+proj=longlat +a=6371000 +b=6371000 +no_defs"))
+
+
+
+
