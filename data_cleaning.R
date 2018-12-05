d9ea38e136a43776d4e8c811df695f848e758a03
Matthew Lowes <matt.lowes@oneacrefund.org>
2018-12-05 09:29:51 -0500

    added subsetYear function to make weather list more manageable

--- data_cleaning.R
+++ data_cleaning.R
@@ -25,3 +25,25 @@ dataDir <- normalizePath(file.path("..", "arc2_weather_data"))
 rawDir <- normalizePath(file.path("..", "arc2_weather_data", "raw_data"))
 load(paste(rawDir, "weatherRasterList.Rdata", sep = "/")) # this is huge! Find a way to break this down to simplify calculations. 
 
+# look at names of rasters << so many but works in concept!
+
+test <- lapply(arcWeatherRaw, function (x) names(x))
+
+
+# SUBSET BY YEAR
+subsetYear <- function(rList, year){
+  # input - list of weather rasters and year as INT.
+  # output - year or years in question
+  year = as.character(year)
+  
+  if(length(year) > 1){
+    year = paste(year, collapse = "|")
+  }
+  
+  nameVector = lapply(rList, function(x) names(x))
+  loc = grep(year, nameVector)
+  res = rList[loc]
+  return(res)
+}
+
+# SUBSET BY MONTH
