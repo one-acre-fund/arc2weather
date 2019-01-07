16b65c5309d88f9e22f326ba8857755453548d51
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-07 13:09:46 -0500

    adds current_list roxy notes

--- R/get_current_list.R
+++ R/get_current_list.R
@@ -1,3 +1,9 @@
+#' gets names of current files already downloaded and worked with
+#'
+#' @param fileDirectory the file location locally where the data names are saved
+#' @return A vector of the file names available already
+
+
 get_current_list <- function(fileDirectory){
   # input: loads the current data. Each time we I access data I'll save a file
   # that has a date and a vector of what we've accessed.
