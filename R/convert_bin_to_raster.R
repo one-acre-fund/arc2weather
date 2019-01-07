5a6f32296c18fbc9a0a58007857c5ee3f12537d0
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-07 16:19:35 -0500

    specifies raster in raster call

--- R/convert_bin_to_raster.R
+++ R/convert_bin_to_raster.R
@@ -8,7 +8,7 @@
 
 
 convert_bin_to_raster <- function(binaryInput, fileUrl){
-  mat <- raster(matrix(binaryInput, nrow = 801, ncol = 751, byrow = TRUE))
+  mat <- raster::raster(matrix(binaryInput, nrow = 801, ncol = 751, byrow = TRUE))
   ### match raster
   extent(mat) = extent(r) ### reference raster
   crs(mat) = crs(r) ### reference raster
