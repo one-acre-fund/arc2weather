ac0ebdf3d7f06f10ac4b15b995c12bdfed357e0d
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-07 18:40:09 -0500

    changes input to rawRasterData for clarity

--- R/extract_weather_data.R
+++ R/extract_weather_data.R
@@ -1,6 +1,6 @@
 #' Combines the full functionality to take the raw inputs and produce data.frame of extracted weather data and dates.
 #'
-#' @param rawData list of raw raster data from Arc2 weather site
+#' @param rawRasterData list of raw raster data from Arc2 weather site
 #' @param gpsFile GPS file, probably from data warehouse, of GPS points for which we want to extract weather values
 #' @param latCol The Latitude column in the gpsFile
 #' @param lonCol The Longitude column in the gpsFile
@@ -9,13 +9,13 @@
 #' add(1, 1)
 #' add(10, 1)
 
-extract_weather_data <- function(rawData,
+extract_weather_data <- function(rawRasterData,
                                gpsFile,
                                latCol,
                                lonCol){
 
   datExtract <- extract_velox_gps(
-    veloxRaster = convert_to_velox(rawData),
+    veloxRaster = convert_to_velox(rawRasterData),
     spdf = convertSpdf(gpsFile, lonCol, latCol))
 
   return(datExtract)
