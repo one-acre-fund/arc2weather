77a6697258a2f1ffc7f323e986c5e356309766c7
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-07 09:31:54 -0500

    adds subset year month notes

--- R/subset_year_month.R
+++ R/subset_year_month.R
@@ -1,5 +1,16 @@
+#' Subset the raster list by month and year combining the subset month and year functions
+#'
+#' @param rList A list of raster in raster format.
+#' @param month A numeric month between 1 and 12 to indicate which month to keep. This can be a vector or range as well.
+#' @param year A numeric year between 1985 and present. A range is also acceptable.
+#' @return A list of rasters for the requested months and years
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+
+
 subset_year_month <- function(rList, year = NULL, month = NULL){
-  
+
   if(is.null(year) && is.null(month)){
     stop("\n Please provide either a year or a month value!")
   } else if (is.null(year) && !is.null(month)){
@@ -9,5 +20,5 @@ subset_year_month <- function(rList, year = NULL, month = NULL){
   } else {
     return(subsetYear(subsetMonth(rList, month), year))
   }
-  
+
 }
