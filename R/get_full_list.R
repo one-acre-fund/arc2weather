af282de408e4f375fe1ee4e8c5dfd0ed3d211a51
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-07 16:01:23 -0500

    changes get full list to directly reference url

--- R/get_full_list.R
+++ R/get_full_list.R
@@ -3,12 +3,12 @@
 #' @param url web url of FTP
 #' @return A vector of the file names available on the FTP
 
-get_full_list <- function(url){
-  # input: data location url
+get_full_list <- function(){
+  # input: no input because the FTP url should remain the same
   # output: full list of available data sets
 
-  h = new_handle(dirlistonly=TRUE)
-  con = curl(url, "r", h)
+  h = curl::new_handle(dirlistonly=TRUE)
+  con = curl::curl("ftp://ftp.cpc.ncep.noaa.gov/fews/fewsdata/africa/arc2/bin/", "r", h)
   fullList = read.table(con, stringsAsFactors=TRUE, fill=TRUE)
   close(con)
   #saveRDS(fullList, file=paste(fullList, todayDate, ".rds", sep = ""))
