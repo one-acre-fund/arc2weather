785af26ac29355f84e4c65aa0bdcb2bfd65f22e4
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-07 16:51:28 -0500

    specify raster library in convert_bin

--- R/convert_bin_to_raster.R
+++ R/convert_bin_to_raster.R
@@ -10,7 +10,7 @@
 convert_bin_to_raster <- function(binaryInput, fileUrl){
   mat <- raster::raster(matrix(binaryInput, nrow = 801, ncol = 751, byrow = TRUE))
   ### match raster
-  extent(mat) = extent(r) ### reference raster
+  raster::extent(mat) = raster::extent(-20.05, 55.05, -40.05, 40.05) ### reference raster
   crs(mat) = crs(r) ### reference raster
   names(mat) = basename(fileUrl)
   return(mat)
