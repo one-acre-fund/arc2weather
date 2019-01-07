42ef0e544568c07006083ae09f628eeeec39b0f3
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-07 09:33:47 -0500

    updates tidy data function

--- R/tidy_arc_weather_data.R
+++ R/tidy_arc_weather_data.R
@@ -1,12 +1,21 @@
+#' Applies some general cleaning to the extracted data before exporting
+#'
+#' @param df The data.frame coming from the full extract process.
+#' @return A data.frame that has some variables cleaned and labels clarified.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+
+
 tidy_arc_weather_data <- function(df){
-  
+
   if(class(df) != "data.frame"){
     stop("\n tidy function wants a data frame as input!")
   }
-  
+
   clean <- df %>%
     mutate(rainfall = ifelse(rainfall < 0, NA, rainfall))
-  
+
   return(clean)
-  
+
 }
