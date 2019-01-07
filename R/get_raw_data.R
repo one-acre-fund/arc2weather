c1d79ca2b1c11489736cd6d773b9276b7537e73b
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-07 16:09:46 -0500

    adds default save directory and today's date

--- R/get_raw_data.R
+++ R/get_raw_data.R
@@ -5,13 +5,19 @@
 #' @return a list of rasters accessed from the FTP. Saves this file to the directory
 
 
-get_raw_data <- function(listToAccess, url = NULL){
+get_raw_data <- function(listToAccess, saveDirectory = NULL, url = NULL){
   # input: list of files that we need to access to have up to date data
   # what it does: turns those into urls and then accesses the data
   # output: saves data to raw_data location
   if(is.null(url)){
-    url = "ftp://ftp.cpc.ncep.noaa.gov/fews/fewsdata/africa/arc2/bin/"
+    url <- "ftp://ftp.cpc.ncep.noaa.gov/fews/fewsdata/africa/arc2/bin/"
   }
+
+  if(is.null(saveDirectory)){
+    saveDirectory <- normalizePath(file.path("..", "arc2_weather_data", "raw_data"))
+  }
+
+  todayDate <- format(Sys.time(), "%Y-%m-%d")
 
   urls =  paste0(url, listToAccess)
 
@@ -21,6 +27,6 @@ get_raw_data <- function(listToAccess, url = NULL){
     return(raw)
   })
 
-  saveRDS(newData, file = paste(rdsDir, paste("weatherRasterList", todayDate, ".rds", sep = ""), sep = "/"))
+  saveRDS(newData, file = paste(saveDirectory, paste("weatherRasterList", todayDate, ".rds", sep = ""), sep = "/"))
 
 }
