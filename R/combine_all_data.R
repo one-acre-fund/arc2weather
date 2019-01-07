078d5fa486fb52e18e015d8348f191ed1e0a6897
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-07 18:35:26 -0500

    init commit of function to combine all rasters for analysis

---
+++ R/combine_all_data.R
@@ -0,0 +1,26 @@
+#' Combines all accessed data into a single list of rasters
+#'
+#' @param directory Where to find the lists of rasters to combine into a single list
+#' @return A list of rasters to put into the convert_to_velox sequence to get it ready for extraction
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+
+
+combine_all_data <- function(directory = NULL){
+
+  if(is.null(directory)){
+    directory <- normalizePath(file.path("..", "arc2_weather_data", "raw_data"))
+  }
+
+  listFiles <- sort(list.files(directory, pattern = "weatherRasterList"))
+
+  # read in data and combine into list
+  fullRasterList <- do.call(c, lapply(listFiles, function(f){
+    tmp <- readRDS(paste(directory, f, sep = "/"))
+    return(tmp)
+  }))
+
+  saveRDS(fullRasterList, file=paste(directory, "full_data_list.rds", sep = "/"))
+
+}
