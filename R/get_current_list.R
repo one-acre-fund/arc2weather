97b7688c0af427573cc0e50ef296cebf1be495d0
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-07 16:04:52 -0500

    adds default to get_current_list

--- R/get_current_list.R
+++ R/get_current_list.R
@@ -4,11 +4,14 @@
 #' @return A vector of the file names available already
 
 
-get_current_list <- function(fileDirectory){
+get_current_list <- function(fileDirectory = NULL){
   # input: loads the current data. Each time we I access data I'll save a file
   # that has a date and a vector of what we've accessed.
   # what it does: accesses the latest file with the list.
   # output: and returns vector of names of what we have.
+  if(is.null(fileDirectory)){
+    fileDirectory <- normalizePath(file.path("..", "arc2_weather_data", "access_lists"))
+  }
 
   listFiles <- list.files(fileDirectory, pattern = "files_downloaded")
   latest <- sort(listFiles)[1]
