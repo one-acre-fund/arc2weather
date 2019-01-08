1df02aeb874d6e6e85bfd92edacc1a15f740226a
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-08 10:34:05 -0500

    change rgdal to sp

--- R/convert_spdf.R
+++ R/convert_spdf.R
@@ -22,12 +22,12 @@ convert_spdf <- function(df, lat = NULL, long = NULL, defaultCRS = NULL){
   }
 
   if(class(df) == "data.frame" && is.null(defaultCRS)) {
-    converted <- sp::SpatialPointsDataFrame(coords = df[,c(long, lat)], data = df, proj4string = rgdal::CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+    converted <- sp::SpatialPointsDataFrame(coords = df[,c(long, lat)], data = df, proj4string = sp::CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
     return(converted)
   }
 
   if(class(df) == "data.frame" && !is.null(defaultCRS)){
-    converted <- sp::SpatialPointsDataFrame(coords = df[,c(long, lat)], data = df, proj4string = rgdal::CRS(defaultCRS))
+    converted <- sp::SpatialPointsDataFrame(coords = df[,c(long, lat)], data = df, proj4string = sp::CRS(defaultCRS))
     return(converted)
   }
 
