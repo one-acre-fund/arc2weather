dcab2c6b33191931b0c5d3c419dd93d5d35daf4a
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-18 16:09:26 -0500

    updates current_list notes and adds new check directory function

--- R/update_current_list.R
+++ R/update_current_list.R
@@ -1,12 +1,13 @@
-#' takes the list of file names and creates a single master list that will be referenced and updated going forward.
+#' takes the list of file names and creates a single master list that will be
+#' referenced and updated going forward. I want this to run every time I update
+#' the arc2 weather data
 #'
 #' @param directory the location where those files live
-#' @return an rds file that has all the data that has been accessed up to this point
+#' @return an rds file that has all the data that has been accessed up to this
+#'   point
 
-update_current_list <- function(directory = NULL){
-  if(is.null(directory)){
-    directory <- normalizePath(file.path("..", "arc2_weather_data", "access_lists"))
-  }
+update_current_list <- function(directory){
+  directory <- check_data_directory(directory)
 
   listFiles <- list.files(directory, pattern = "files_downloaded")
   fullList <- do.call(rbind, lapply(listFiles, function(f){
