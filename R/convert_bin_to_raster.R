5884e13457c2f4698d672649844c0af7dc750db2
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-18 15:19:10 -0500

    specifies crs of raster data

--- R/convert_bin_to_raster.R
+++ R/convert_bin_to_raster.R
@@ -10,7 +10,7 @@ convert_bin_to_raster <- function(binaryInput, fileUrl){
   mat <- raster::raster(matrix(binaryInput, nrow = 801, ncol = 751, byrow = TRUE))
   ### match raster
   raster::extent(mat) <- raster::extent(-20.05, 55.05, -40.05, 40.05) ### reference raster
-  crs(mat) <- crs(r) ### reference raster
+  crs(mat) <- "+proj=longlat +a=6371000 +b=6371000 +no_defs" ### crs of data
   names(mat) <- basename(fileUrl)
   return(mat)
 }
