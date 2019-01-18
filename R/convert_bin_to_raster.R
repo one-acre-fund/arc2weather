ece33adc42c6e3de39f248ea1205d96e803ea431
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-18 14:49:48 -0500

    checking raster conversion and alignment during download

--- R/convert_bin_to_raster.R
+++ R/convert_bin_to_raster.R
@@ -9,8 +9,8 @@
 convert_bin_to_raster <- function(binaryInput, fileUrl){
   mat <- raster::raster(matrix(binaryInput, nrow = 801, ncol = 751, byrow = TRUE))
   ### match raster
-  raster::extent(mat) = raster::extent(-20.05, 55.05, -40.05, 40.05) ### reference raster
-  crs(mat) = crs(r) ### reference raster
-  names(mat) = basename(fileUrl)
+  raster::extent(mat) <- raster::extent(-20.05, 55.05, -40.05, 40.05) ### reference raster
+  crs(mat) <- crs(r) ### reference raster
+  names(mat) <- basename(fileUrl)
   return(mat)
 }
