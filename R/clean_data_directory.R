53e27416109f7119a38f971954358259942d9db2
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-22 15:08:01 -0500

    adds function to clean data directory

---
+++ R/clean_data_directory.R
@@ -0,0 +1,15 @@
+#' clean raw data directory of dated files.
+#'
+#' @param dir directory where raw data lives
+#' @param listToAccess the list of files to access from get_new_additions
+#' @inheritParams get_raw_data()
+#' @return nothing. It saves a single data file and deletes the other files.
+
+
+clean_data_directory <- function(dir, pattern = "weatherRasterList"){
+  list_to_remove <- list.files(dir, pattern)
+
+  do.call(file.remove, list(list_to_remove))
+
+
+}
