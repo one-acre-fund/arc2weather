0dd0e309ec878c16785417d85dc324dd0d990a21
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-25 10:44:38 -0500

    adds example for api

--- R/arc2_api_download.R
+++ R/arc2_api_download.R
@@ -3,8 +3,7 @@
 #' @param vector_of_dates A list of raster in velox format. Shoudl be in YYYY-MM-DD format
 #' @return An spdf with the same format as the velox raster data. We can re-use the dates list again without passing it through the function as returned output.
 #' @examples
-#' add(1, 1)
-#' add(10, 1)
+#' arc2_api_download(c("2019-01-01", "2019-01-02"))
 
 arc2_api_download <- function(list_of_dates){
 
