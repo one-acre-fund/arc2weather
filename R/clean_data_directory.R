fa82f8e2cb259c626a24a300f99fbefbf3e3236b
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-29 15:51:52 -0500

    fixes inheret params call

--- R/clean_data_directory.R
+++ R/clean_data_directory.R
@@ -2,7 +2,7 @@
 #'
 #' @param dir directory where raw data lives
 #' @param listToAccess the list of files to access from get_new_additions
-#' @inheritParams get_raw_data()
+#' @inheritParams get_raw_data
 #' @return nothing. It saves a single data file and deletes the other files.
 
 
